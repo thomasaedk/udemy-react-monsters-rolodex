330f68fa3535af5aa387ec17d9da5c44a049113e
Thomas Aegidiussen <thomasaedk@gmail.com>
2019-09-26 16:48:35 +0200

    Updates

---
+++ precache-manifest.1e7a2d8e19885bcee76197e612ff7930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6667bebfa2c925f873c71090e9c0f25e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c131818e6c706cf29955",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "31ebe7399c153308fd34",
+    "url": "/static/js/2.27fb1107.chunk.js"
+  },
+  {
+    "revision": "c131818e6c706cf29955",
+    "url": "/static/js/main.399d484d.chunk.js"
+  },
+  {
+    "revision": "337600726fc3c2c90cff",
+    "url": "/static/js/runtime-main.d2cea1ee.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
